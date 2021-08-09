a24690a66afb55de75d77c6959fd8805089668e1
Jvala2 <joevalancy@gmail.com>
2021-08-09 16:30:19 -0400

    typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,13 +10,22 @@ type Book {
     title: String
 
   }
+  type User {
+      _id: ID
+      username: String
+      email: String
+      password: String
+      savedbooks: [Book]
+  }
   type Query {
     allBooks: [Book]!
     getBooks: BookID: ID!): Book
     getAllUser: [User]!
   }
   type Mutation {
-    createBook (user: String!): user
+    createBook (user: String!, authors: String!, description: String!, image: String!, link: String!, title: String!): user
+    removeBook (user: String!, BookID: ID!): user
+    createUser (username: String!, email: String!, password: String!): user
   }
 
 `;
