8c24e954bf6fd6ae59f3127d662f1e4f12d0ea7a
Jvala2 <joevalancy@gmail.com>
2021-08-09 16:39:36 -0400

    search

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,22 @@
 import { gql } from '@apollo/client';
 
 export const ADD_BOOK = gql`
-    mutation createBook($tech1: String!, $tech2: String!) {
-        createMatchup(tech1: $tech1, tech2: $tech2) {
+    mutation createBook($bookData: BookInput!) {
+        createBook(bookData: $bookData) {
+            _id
+            username
+            email
+            createdBooks {
+                bookId
+                authors
+            }
+        }
 
         }
     }`
 
 
-export const ADD_PROFILE = gql`
+export const ADD_USER = gql`
 
         }
     }
