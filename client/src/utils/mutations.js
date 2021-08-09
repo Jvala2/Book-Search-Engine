1adc850c89446fe36e0e01e9185e0f0a324ce4cc
Jvala2 <joevalancy@gmail.com>
2021-08-09 15:42:07 -0400

    mutations are wrong, deleted

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const ADD_BOOK = gql`
+    mutation createBook($tech1: String!, $tech2: String!) {
+        createMatchup(tech1: $tech1, tech2: $tech2) {
+
+        }
+    }`
+
+
+export const ADD_PROFILE = gql`
+
+        }
+    }
+`; 
