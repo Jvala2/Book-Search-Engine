6b34c60f222d7b458f5aae204d80b4bd57858549
Jvala2 <joevalancy@gmail.com>
2021-08-09 15:30:39 -0400

    resolvers updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,38 @@ const { Book, Index, User } = require('../models');
 
 const resolvers = {
 
-
+    allbooks: async () => {
+        return await book.find();
+      },
+  
+      getbook: async (parent, { bookId }) => {
+        return await book.findOne({ _id: bookId });
+      },
+  
+      getAllTech: async () => {
+        return await Tech.find();
+      },
+  
+    },
+  
+    Mutation: {
+      createbook: async (parent, { user }) => {
+        return await book.create({ user });
+      },
+      createUser: async (parent, { bookId, userSchema }) => {
+        return await book.findOneAndUpdate(
+          { _id: bookId },
+          {
+           $inc: { [`user${userSchema}`]: 1 }, 
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      },
+  
+    },
 
 };
 
